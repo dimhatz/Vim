289a5d0f0e524b4b9517a836eaa67affd5fa5e31
dimhatz <dimhatz@users.noreply.github.com>
2024-04-05 19:59:23 +0300

    disable on text changed listener

--- extensionBase.ts
+++ extensionBase.ts
@@ -146,6 +146,7 @@ export async function activate(context: vscode.ExtensionContext, handleLocal: bo
   );
 
   registerEventListener(context, vscode.workspace.onDidChangeTextDocument, async (event) => {
+    return;
     if (event.document.uri.scheme === 'output') {
       // Without this, we'll get an infinite logging loop
       return;
@@ -194,6 +195,7 @@ export async function activate(context: vscode.ExtensionContext, handleLocal: bo
 
     const mh = ModeHandlerMap.get(event.document.uri);
     if (mh) {
+      // @ts-expect-error unreachable code
       contentChangeHandler(mh);
     }
   });
